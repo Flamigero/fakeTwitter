ed6e4d70069b23b7e2a1208d6ba96110686a6a22
Carlos <carlos@carlos.com>
2021-04-14 15:12:56 +0200

    Create base and login template

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,9 @@ Users URLs
 # Django
 from django.urls import path
 
+# Views
+from .views import login_view
+
 urlpatterns = [
-    
+    path('login/', login_view, name='login')
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
 """
 Users views
 """
+
+# Django
+from django.shortcuts import render, redirect
+
+def login_view(request):
+    """Login view"""
+
+    return render(request, 'users/login.html')
