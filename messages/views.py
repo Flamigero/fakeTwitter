966eba5252d7710adf0b91e4b68971c258cc8837
Carlos <carlos@carlos.com>
2021-04-15 14:49:38 +0200

    Fix retweet and create login view

--- messages/views.py
+++ messages/views.py
@@ -12,7 +12,7 @@ def feed(request):
     tweets = Message.objects.raw("""
         select id, text, user_id, retweet_id from twitter_messages_message where retweet_id IS NULL
         union
-        select t.id, m.text, m.user_id, t.retweet_id from twitter_messages_message t inner join twitter_messages_message m on t.retweet_id = m.id
+        select t.id, m.text, t.user_id, t.retweet_id from twitter_messages_message t inner join twitter_messages_message m on t.retweet_id = m.id
     """)
 
     return render(request, 'feed.html', {
--- users/views.py
+++ users/views.py
@@ -4,11 +4,21 @@ Users views
 
 # Django
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
 def login_view(request):
     """Login view"""
     if request.method == 'POST':
         email = request.POST['email']
         password = request.POST['password']
+
+        user = authenticate(request, email=email, password=password)
+        if user:
+            login(request, user)
+            return redirect('feed')
+        else:
+            return render(request, 'users/login.html', {
+                'error': 'Invalid user or password'
+            })
 
     return render(request, 'users/login.html')
