0a5fbb1283ce79946cbeb32883f03e32867b5146
Carlos Nuñez Jimenez <cnunez@GRUPO-MIRAMAR.LOCAL>
2021-05-10 11:56:54 +0200

    Init tests

---
+++ tests/fakeTwitter_main.py
@@ -0,0 +1,39 @@
+""" FakeTwitter class tests """
+
+# Selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class FakeTwitterPage(object):
+    def __init__(self, os):
+        if os == "windows":
+            self.driver = webdriver.Chrome(executable_path = '../chromedriver.exe')
+        else:
+            self.driver = webdriver.Chrome(executable_path = '../chromedriver.exe')
+
+        self.url = 'http://localhost:8000/users/login/'
+
+    def open(self):
+        """ Open web """
+        self.driver.get(self.url)
+        self.driver.maximize_window()
+
+    def quit(self):
+        self.driver.quit()
+
+    def login(self):
+        """ Make login """
+        email = self.driver.find_element_by_name('email')
+        password = self.driver.find_element_by_name('password')
+        #submit = self.driver.find_element_by_xpath('/html/body/section/form/input[4]')
+
+        email.click()
+        email.clear()
+        email.send_keys('testing@testing.com')
+
+        password.click()
+        password.clear()
+        password.send_keys('testing')
+        password.submit()
---
+++ tests/fakeTwitter_tests.py
@@ -0,0 +1,23 @@
+""" FakeTwitter tests """
+
+# Test
+import unittest
+
+# Model
+from fakeTwitter_main import FakeTwitterPage
+
+class FakeTwitterTest(unittest.TestCase):
+
+    def setUp(self):
+        os = 'windows'
+        self.fakeTwitter = FakeTwitterPage(os)
+
+    def login_test():
+        self.fakeTwitter.open()
+        self.fakeTwitter.login()
+
+    def tearDown(self):
+        self.fakeTwitter.quit()
+
+if __name__ == '__name__':
+    unittest.main(verbosity=2)        
